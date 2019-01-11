373e405fc61e718b3c16400d6a20e71b6123ed1f
Ruben Cavieres <ruben.cavieres@multicaja.cl>
2019-01-11 17:43:57 -0300

    Sending JSON message.

--- 01/pub-str.js
+++ 01/pub-str.js
@@ -3,7 +3,7 @@ var stan = require('node-nats-streaming').connect('test-cluster', 'test-2');
 stan.on('connect', function () {
  
   // Simple Publisher (all publishes are async in the node version of the client)
-  stan.publish('order.payment.rejected', 'Hasta la vista.', function(err, guid){
+  stan.publish('order.payment.rejected', '{ "status": "rejected", "order_id": 165 }', function(err, guid){
     if(err) {
       console.log('publish failed: ' + err);
     } else {
